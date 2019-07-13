68c7f47b819278e6ea794a7ccf07c6ed369258d0
cnboker <coobots@gmail.com>
2019-07-13 11:45:30 +0800

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,30 @@
 (function($) {
+    var key = atob(getUrlParameter('key'))
+    var id = getParameter(key,'id')
+    var url = getParameter(key, 'url')
+    if(id && id.length === 24){
+        $.post(url,{
+            id
+        })
+    }
     $('.carousel').carousel(); 
-})
+})($)
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+    getParameter(sPageURL)
+};
+
+function getParameter(sPageURL, key){
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
